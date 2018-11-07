ea91e41e27c2ba010afbc2816f52dd8ef8496eb6
lcy <lcy@aishk.com>
2018-11-07 10:15:23 +0800

    AIS 20181107 01

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -19,6 +19,7 @@
  */
 
 /** IE9, IE10 and IE11 requires all of the following polyfills. **/
+ import 'core-js/shim';
  import 'core-js/es6/symbol';
  import 'core-js/es6/object';
  import 'core-js/es6/function';
@@ -38,7 +39,7 @@
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /** IE10 and IE11 requires the following for the Reflect API. */
-// import 'core-js/es6/reflect';
+ import 'core-js/es6/reflect';
 
 
 /** Evergreen browsers require these. **/
@@ -66,7 +67,7 @@
  * in IE/Edge developer tools, the addEventListener will also be wrapped by zone.js
  * with the following flag, it will bypass `zone.js` patch for IE/Edge
  */
-// (window as any).__Zone_enable_cross_context_check = true;
+ (window as any).__Zone_enable_cross_context_check = true;
 
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
