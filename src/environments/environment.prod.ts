701b07470dee9896324d2eaffe75661bc51d5f7e
lcy <lcy@aishk.com>
2018-12-04 08:43:14 +0800

    update env api url to short

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,8 +4,8 @@ export const environment = {
 
 export class EnvironmentConstant {
   // public static API_URL = 'http://10.20.100.211:80/api';
-  public static AVATAR = 'https://otcfiretesting.space/avatar';
-   public static API_URL = 'https://otcfiretesting.space/api';
+  public static AVATAR = '/avatar';
+   public static API_URL = '/api';
    public static BG_URL = 'https://admin-api.otcfiretesting.space';
   public static API_URL_V1 = EnvironmentConstant.API_URL;
 
