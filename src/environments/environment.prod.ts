0f89aa811e510be18b16e75108f14f1e2a5a6190
AIS_LCY <lcy@aishk.com>
2019-01-23 11:10:57 +0000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,6 +7,6 @@ export class EnvironmentConstant {
   public static API_URL = '/api';
   public static API_URL_V1 = EnvironmentConstant.API_URL;
   public static MINIO_URL_V1 = '';
-   public static CHATING_TOPIC_KEYWORD = '_dev01'; // for master branch
-   // public static CHATING_TOPIC_KEYWORD = '_prd01'; // for prd branch
+  // public static CHATING_TOPIC_KEYWORD = '_dev01'; // for master branch
+    public static CHATING_TOPIC_KEYWORD = '_prd01'; // for prd branch
 }
