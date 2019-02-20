0323bb229528e289401c8674518c01c0f1b8bf60
Alexander Ho <usagii@link.cuhk.edu.hk>
2019-02-20 08:10:50 +0000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
 };
 
 export class EnvironmentConstant {
-  public static AVATAR = '/avatar';
-  public static API_URL = '/api';
+  public static AVATAR = 'https://minio.koin.minus.cafe/avatar';
+  public static API_URL = 'https://api.koin.minus.cafe/api';
   public static API_URL_V1 = EnvironmentConstant.API_URL;
   public static MINIO_URL_V1 = '';
   // public static CHATING_TOPIC_KEYWORD = '_dev01'; // for master branch
