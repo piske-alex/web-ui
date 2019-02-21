006207d1c17da5454171dd59d70b6e9d67a81cf1
Alexander Ho <usagii@link.cuhk.edu.hk>
2019-02-21 10:55:18 +0800

    minio url + redirect for netlify test 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export class EnvironmentConstant {
   public static AVATAR = 'https://minio.koin.minus.cafe/avatar';
   public static API_URL = 'https://api.koin.minus.cafe/api';
   public static API_URL_V1 = EnvironmentConstant.API_URL;
-  public static MINIO_URL_V1 = 'https://minio.koin.minus.cafe/';
+  public static MINIO_URL_V1 = 'https://minio.koin.minus.cafe';
   // public static CHATING_TOPIC_KEYWORD = '_dev01'; // for master branch
    public static CHATING_TOPIC_KEYWORD = '_prd01'; // for prd branch
 }
